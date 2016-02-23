611e3818b04a6b68d3d6dfed0bd9b3cfbe51b37d
asagurachan <asagurachan@gmail.com>
2016-02-24 00:08:47 +0800

    Update

--- app/src/main/java/com/stella/pals/frontend/message_groups/MessageGroupsFragment.java
+++ app/src/main/java/com/stella/pals/frontend/message_groups/MessageGroupsFragment.java
@@ -24,25 +24,8 @@ import io.realm.Realm;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link groups.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link groups#newInstance} factory method to
- * create an instance of this fragment.
- */
 @EFragment(R.layout.fragment_message_groups)
 public class MessageGroupsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     @App
     BaseApplication application;
 
@@ -59,31 +42,9 @@ public class MessageGroupsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessageGroups.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MessageGroupsFragment newInstance(String param1, String param2) {
-        MessageGroupsFragment fragment = new MessageGroupsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         realm = Realm.getDefaultInstance();
     }
